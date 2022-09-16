ce901798b28ee2aa0d830b265c58fe9c2bcd34a2
yadavravi043 <ravi18016925@gmail.com>
2022-09-16 10:29:33 +0530

    url for api

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-export const host = "http://localhost:5000";
-// export const host = "https://chatapp043.herokuapp.com/";
+// export const host = "http://localhost:5000";
+export const host = "https://my-chat-app-ravi.herokuapp.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
